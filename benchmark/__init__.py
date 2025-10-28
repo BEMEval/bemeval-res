253a00f07706742f80567670d04f204229bd31c5
Yiyuan Jia <yj.github@gmail.com>
2025-10-27 21:01:59 -0600

    Setup initial folder sturctures.

---
+++ benchmark/__init__.py
@@ -0,0 +1,7 @@
+"""
+BEMEval Benchmark Package
+
+This package contains evaluation scripts and utilities for the BEMEval benchmark.
+"""
+
+__version__ = "0.1.0"
---
+++ benchmark/evaluate.py
@@ -0,0 +1,5 @@
+"""
+Evaluation script for BEMEval benchmark.
+
+This script provides functionality to evaluate models on the BEMEval benchmark tasks.
+"""
